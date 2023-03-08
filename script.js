ae150db3266b7db9972fff4625b0f82e217ecd8f
najikh2002 <hizbullahnajihan@gmail.com>
2023-03-08 16:55:03 +0700

    feat: max count feature

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // buttons and counter text
 const counter = document.querySelector("#counter");
 const btns = document.querySelectorAll(".btn");
+const audio = new Audio("audio.mp3");
+const set = document.querySelector(".set");
 
-//
-
+// variabel nilai dan maks
 let count = 0;
+let maxx = 0;
+
+// pengamnilan nilai input dan fungsi tombol set
+document
+  .querySelector('input[type="number"]')
+  .addEventListener("input", function () {
+    const max = this.value;
+    set.addEventListener("click", () => {
+      maxx = max;
+      count = 0;
+    });
+  });
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -12,8 +25,12 @@ btns.forEach((btn) => {
 
     if (styles.contains("increase")) {
       count++;
+      if (maxx > 0 && count >= maxx) {
+        audio.play();
+      }
     } else {
       count = 0;
+      maxx = 0;
     }
 
     counter.textContent = count;
